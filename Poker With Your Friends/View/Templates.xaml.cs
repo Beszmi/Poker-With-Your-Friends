b7e0e601f6dd799d106ab6aea683fb4465dd6916
b_barnabas <46139601+Beszmi@users.noreply.github.com>
2026-08-05 14:25:54 +0200

    moved templates out of the page's code

---
+++ Poker With Your Friends/View/Templates.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml;
+
+namespace Poker_With_Your_Friends;
+
+public partial class Templates : ResourceDictionary
+{
+    public Templates()
+    {
+        InitializeComponent();
+    }
+}
